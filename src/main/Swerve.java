3f580a1921290c091c017a878c28761861717819
nicyoloswag <nlabadan18@deerfield.edu>
2015-01-25 16:08:27 -0500

    Made main class
    swag

---
+++ src/main/Swerve.java
@@ -0,0 +1,38 @@
+package main;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Swerve extends org.newdawn.slick.BasicGame{
+
+	public static void main(String[] args) throws SlickException{
+		AppGameContainer gc = new AppGameContainer(new Swerve());
+		gc.start();
+	}
+
+	public Swerve() {
+		super("SWAG");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
