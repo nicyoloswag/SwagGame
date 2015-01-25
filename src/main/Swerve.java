ce0ac4603b431078db47e099e5ccfca307816a99
Markus Feng <zzz1112222595@hotmail.com>
2015-01-25 17:00:16 -0500

    Added player, movement, screen borders

--- src/main/Swerve.java
+++ src/main/Swerve.java
@@ -1,14 +1,25 @@
 package main;
 
 import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class Swerve extends org.newdawn.slick.BasicGame{
 
+	private static final int WIDTH = 750;
+	private static final int HEIGHT = 650;
+
+	private int x = 50;
+	private int y = 75;
+	private int radius = 10;
+	private int speed = 10;
+
 	public static void main(String[] args) throws SlickException{
 		AppGameContainer gc = new AppGameContainer(new Swerve());
+		gc.setDisplayMode(WIDTH, HEIGHT, false);
 		gc.setTargetFrameRate(60);
 		gc.start();
 	}
@@ -19,9 +30,10 @@ public class Swerve extends org.newdawn.slick.BasicGame{
 	}
 
 	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
-
+	public void render(GameContainer arg0, Graphics graphics) throws SlickException {
+		graphics.setBackground(Color.white);
+		graphics.setColor(Color.blue);
+		graphics.fillOval(x, y, radius*2, radius*2);
 	}
 
 	@Override
@@ -30,10 +42,36 @@ public class Swerve extends org.newdawn.slick.BasicGame{
 
 	}
 
-	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
-		// TODO Auto-generated method stub
 
+	@Override
+	public void update(GameContainer gc, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		Input input = gc.getInput();
+		if(input.isKeyDown(Input.KEY_S)){
+			y += speed;
+		}
+		if(input.isKeyDown(Input.KEY_W)){
+			y -= speed;
+		}
+		if(input.isKeyDown(Input.KEY_A)){
+			x -= speed;
+		}
+		if(input.isKeyDown(Input.KEY_D)){
+			x += speed;
+		}
+
+		if(x < 0){
+			x = 0;
+		}
+		else if(x+radius*2 > WIDTH){
+			x = WIDTH-radius*2;
+		}
+		if(y < 0){
+			y = 0;
+		}
+		else if(y+radius*2 > HEIGHT){
+			y = HEIGHT-radius*2;
+		}
 	}
 
 }
